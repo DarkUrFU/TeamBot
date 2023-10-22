d7ef8a1720485cf6148bfb1c40eb8eb2349b76d2
SAlexiy <serovalexey@outlook.com>
2023-10-22 20:46:12 +0500

    initial

---
+++ src/main/java/com/darkurfu/teambot/TeamBotApplication.java
@@ -0,0 +1,13 @@
+package com.darkurfu.teambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/darkurfu/teambot/TeamBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkurfu.teambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
